59d6514916a9c25b500822ddedb95d581429ef14
Your Name <41554290+dschrier34@users.noreply.github.com>
2021-03-15 11:32:16 -0400

    slider updated

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav_link')
+const navLinks = document.querySelectorAll('.nav_link');
+const slides=document.querySelector('.project-slider-items').children;
+const nextSlide=document.querySelector(".right-slide");
+const prevSlide=document.querySelector(".left-slide");
+let index = 0;
+let totalSlides=slides.length;
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,4 +15,31 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+nextSlide.onclick=function () {
+    next("next");
+}
+prevSlide.onclick=function () {
+    next("prev");
+}
+
+function next(direction){
+    if(direction=="next"){
+        index++;
+        if(index==totalSlides){
+            index=0;
+        }
+    }else {
+        if(index==0){
+            index=totalSlides-1;
+        }else {
+            index--;
+        }
+    }
+    for(i=0; i<slides.length; i++){
+        slides[i].classList.remove("active");
+    }
+    
+    slides[index].classList.add("active");
+}
 
